0fb2aa8a4e921241168141bf4c0230fdd210080b
leeyew <leeyew@sheridancollege.ca>
2019-05-23 14:09:13 -0400

    card generated lines added added code for magic hand

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,8 +10,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * //modifier Yewon Lee
  */
+
+ import java.util.*;
 public class CardTrick {
+    
+
     
     public static void main(String[] args)
     {
@@ -20,9 +25,24 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(Math. random() * 13 );
+            c.setSuit(Card.SUITS(Math. random() * 3) ;
         }
+        
+        
+        Scanner input  = new Scanner (System.in);
+        
+        
+        System.out.println ("Guess your card starting with value");
+        int valueGuess = input.nextInt();
+        System.out.println ("Guess your card with suit 1-Hearts, 2-Diamonds, 3-Spades,4-Clubs");
+        int suitGuess = input.nextInt();
+        
+        
+        System.out.println ("your guess for the value was "+ valueGuess + " and for suit was " + suitGuess);
+        System.out.println ("Magic hand was " + c.getValue + " and " + c.getSuit);
+        
+        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
