c61827c12fe558733692537c812a64f071ce585d
dhwanipatel1211 <dhwanipatel1211@gmail.com>
2019-05-23 14:35:46 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -17,12 +18,36 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        
+        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue((int)(Math.random()* 13 + 1));
+            c.setSuit(Card.SUITS[(int) (Math.random() *  4)]);
+            
+            magicHand[i] = c;
+            
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        
+        for(int i = 0; i < magicHand.length; i++){
+            System.out.println(magicHand[i].getValue() + " of " + magicHand[i].getSuit());
+        }
+        
+        Scanner input = new Scanner(System.in);
+        
+        Card cardObject = new Card();
+        
+        System.out.println("Enter a number between 1 and 13: ");
+        int userInput = input.nextInt();
+        
+        System.out.println("Enter a suit: ");
+        String suitInput = input.next();
+        
+        System.out.println("user picked Card: " + cardObject.getValue() + " of "
+                         + cardObject.getSuit());
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
