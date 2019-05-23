7e6c0557c1bc8767a9d6d0c55043e82a1a795289
rasandee <50712421+rasandee@users.noreply.github.com>
2019-05-23 14:10:41 -0400

    Hard-coded luckyCard

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -37,6 +37,10 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        Card luckycard=new Card();
+        luckyCard.setValue(6);
+        luckyCard.setSuit("Hearts");
+        
     }
     
 }
