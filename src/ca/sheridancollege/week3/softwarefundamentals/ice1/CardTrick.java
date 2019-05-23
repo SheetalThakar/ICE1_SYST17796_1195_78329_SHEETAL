657b9c08dcbe8d23b803c614403b8d02fa466243
Manpreet Manni <mannimanpreet14@gmail.com>
2019-05-23 13:39:30 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,14 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @Modifier Manpreet Manni 991540126
  */
 public class CardTrick {
     
@@ -20,11 +23,31 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random() * 13 + 1));
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner(System.in);
+        
+        // taking inputs
+        System.out.print("Enter a Suit: ");
+        String userEnteredSuit = input.next();
+        
+        System.out.print("Enter a number (1 - 13): ");
+        int userEnteredValue = input.nextInt();
+        
+        // object creation
+        Card newCardObject = new Card();
+        newCardObject.setSuit(userEnteredSuit);
+        newCardObject.setValue(userEnteredValue);
+        
+        // displaying result
+        System.out.println("You picked: " + newCardObject.getValue() + " of "
+                            + newCardObject.getSuit());
+        
         // and search magicHand here
         //Then report the result here
     }
