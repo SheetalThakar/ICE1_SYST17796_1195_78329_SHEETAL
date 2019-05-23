3db4dec5264c2ee1520a16fe14d6afde8cd67ccb
Kushalpreet Singh <sinkusha@sheridancollege.ca>
2019-05-23 14:39:39 -0400

    finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,27 +4,45 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.*;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @modifier Kushalpreet Singh
+ * Student number=991541546
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random rand=new Random();
+        Scanner input = new Scanner(System.in);
+        
+        
         
         for (int i=0; i<magicHand.length; i++)
         {
+            int cardNumber=rand.nextInt(14)+1;
+            int suitNumber=rand.nextInt(4);
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(cardNumber);
+            c.setSuit(Card.SUITS[suitNumber]);
+            System.out.println("your card suit "+ c.getSuit());    
+            System.out.println("your card number"+ c.getValue());    
+
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
+
+        System.out.println("Enter the suit for your card");
+        String userGuess=input.nextLine();
+        System.out.println("enter the number for your card");
+        int num=input.nextInt();
+
+           System.out.println("user card number "+num);
+           System.out.println("user suits number "+userGuess );
         // and search magicHand here
         //Then report the result here
     }
