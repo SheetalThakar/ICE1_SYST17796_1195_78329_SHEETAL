7fef319cbeed91d28ac13f65ca5e6026f9f77d81
dhwanipatel1211 <50713344+dhwanipatel1211@users.noreply.github.com>
2019-05-23 14:38:39 -0400

    Hard-Coded LuckyCard

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -49,6 +49,10 @@ public class CardTrick {
         System.out.println("user picked Card: " + cardObject.getValue() + " of "
                          + cardObject.getSuit());
         
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Heart");
+        
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
