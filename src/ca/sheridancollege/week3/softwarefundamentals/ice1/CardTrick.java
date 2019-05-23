991bdc7b0dd029ebbcb48db42bc904ea00caaa50
Manjot_Singh <manjot.singh581@gmail.com>
2019-05-23 14:16:33 -0400

    Finished adding code for 3 and 4 to CardTrick.java.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -32,7 +32,7 @@ public class CardTrick {
                 System.out.println("The generated cards are: ");
         for(int i=0; i<magicHand.length;i++)
         {
-            System.out.println("Card"+(i+1)+"is "+magicHand[i].getValue()+"of"
+            System.out.println("Card"+(i+1)+"is "+magicHand[i].getValue()+" of "
             +magicHand[i].getSuit());
         }
         
