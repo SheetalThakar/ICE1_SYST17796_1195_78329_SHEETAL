8ae39d6683eed79129f6831ed090e7351cb130c1
Gurminderdeep <gurmingu@sheridancollege.ca>
2019-05-23 14:08:07 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,12 +5,17 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * Modifier Gurminderdeep
+ * Student Number: 991542689
  */
+
 public class CardTrick {
     
     public static void main(String[] args)
@@ -21,12 +26,28 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            int min = 1;
+            int  max = 13;
+            c.setValue((int) ((Math.random()*(max-min+1))+min));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            min = 0;
+            max = 3;
+            c.setSuit(Card.SUITS[((int) ((Math.random()*(3-0+1))+0))]);
+            System.out.println(c.getValue()+" of "+c.getSuit());
         }
         
+       
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pick a card value > ");
+        int cardValuePicked = scan.nextInt();
+        System.out.println("Pick a card suit(0 to 3) >");
+        int cardSuitPicked = scan.nextInt();
+        Card userCard = new Card();
+        userCard.setValue(cardValuePicked);
+        userCard.setSuit(Card.SUITS[cardSuitPicked]);
+        System.out.println("The card You picked was "+ userCard.getValue()+" of "+userCard.getSuit());
+        
     }
     
 }
