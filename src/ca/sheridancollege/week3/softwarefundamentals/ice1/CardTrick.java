c12a6efd909c98cb4256f73f50dfc226bb3de11a
Pallavi <sharm640@sheridancollege.ca>
2019-05-23 14:12:19 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @Pallavi Sharma-991548375
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
@@ -22,11 +24,37 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13)+1);
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i]=c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        
+        
+        
+       
+        
+        System.out.println("The generated cards were ");
+        for(int i = 0 ;i<magicHand.length;i++)
+        {
+            System.out.println("Card "+(i+1)+" is "+magicHand[i].getValue()+" of "+magicHand[i].getSuit());
+        }
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a card number:");
+        int card=sc.nextInt();
+        System.out.println("Enter a suit number");
+        String suit=sc.next();
+        Card c=new Card();
+        c.setValue(card);
+        c.setSuit(suit);
+        System.out.println("The new card is: "+c.getValue()+" of "+c.getSuit());
+        
+        
+        
     }
     
 }
