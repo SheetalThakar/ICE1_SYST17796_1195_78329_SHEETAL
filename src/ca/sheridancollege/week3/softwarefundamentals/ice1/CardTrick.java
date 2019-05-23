e9985312132ab879582937120caebe07f2636de7
janhvisharma <janhvisharma@icloud.com>
2019-05-23 14:17:48 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,30 +1,45 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * JANHVI SHARMA
+ * 991542217
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author sheetal
+ * @author janhvisharma
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        int a;
+        String b;
+      
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*3+1)]);
+            b=c.getSuit();
+            a=c.getValue();
+            System.out.println(a+"  "+b);
+            magicHand[i]=c;
         }
-        
+        Scanner input= new Scanner(System.in);
+       
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter a card number: ");
+        int cardNumber= input.nextInt();
+        input.nextLine();
+        System.out.println("Enter card suit: ");
+        String cardSuit= input.nextLine();
+       
+        System.out.println("Your card is: "+cardNumber+"  "+cardSuit);
+        
         // and search magicHand here
         //Then report the result here
     }
