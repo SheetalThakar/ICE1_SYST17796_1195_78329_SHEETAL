35a350f7fdab7a24a861c6c513776f3f87bba85c
Manpreet Kaur <kaur6668@sheridancollege. ca>
2019-05-23 14:00:56 -0400

    - “Finished adding code for 3 and 4 to CardTrick.java”.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,12 +10,28 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @modifier Manpreet kaur (991544958)
  */
+
+import java.util.Scanner;
+
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        
+        int ranCard = (int) (Math.random()*13 +1);
+        int ranSuit = (int) (Math.random()*3);
+       
+        System.out.println(" The random card is :" + ranCard);
+        System.out.println(" The random suit is: " + ranSuit);
+        
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Enter any number between 1 to 7 card: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Enter any number between 0 to 4 for suit: ");
         
         for (int i=0; i<magicHand.length; i++)
         {
