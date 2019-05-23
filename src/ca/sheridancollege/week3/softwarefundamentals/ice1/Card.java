5a3ee70e72a9e4e3fa24dd3fa92cb069945b3f3a
Lovejeshbangar <bangerl@sheridancollege.ca>
2019-05-23 14:30:37 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -13,11 +13,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author sheetal
+ * @modifier lovejesh
  */
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13 
+   private String card;
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
@@ -49,5 +51,4 @@ public class Card {
     }
    
    
-    
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,23 +10,49 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @modifier lovejesh
  */
+import java.util.*;
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        String card;
+        Scanner input=new Scanner(System.in);
+        CardTrick self= new CardTrick();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(self.numberGenerator1());
+            c.setSuit(Card.SUITS[self.numberGenerator2()]);
+            
+            magicHand[i]=c;
         }
-        
+        for(int i=0;i<magicHand.length;i++)
+        {
+            System.out.println(magicHand[i].getValue()+" OF "+ magicHand[i].getSuit());
+        }
+        System.out.println("Enter a value of card(1-13?");
+        int a=input.nextInt();
+         
+        System.out.println("Enter the name of card 0,1,2,3 for hearts, diamonds, spades and club ");
+        int b=input.nextInt();
+         System.out.println("The user chose "+Card.SUITS[b]+" of " + a);
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
     }
+    public int numberGenerator1()
+    {
+        Random rand =new Random();
+        return (rand.nextInt(12)+1);
+    }
+    public int numberGenerator2()
+    {
+        Random rand =new Random();
+        return (rand.nextInt(4));
+    }
+    
     
 }
