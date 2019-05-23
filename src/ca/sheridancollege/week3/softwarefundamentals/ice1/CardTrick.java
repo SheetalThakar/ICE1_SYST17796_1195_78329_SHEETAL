dfa37262b80a639e7d572308d4b57c4f03492968
jashandeepkaur007 <50712708+jashandeepkaur007@users.noreply.github.com>
2019-05-23 14:44:40 -0400

    Hard-coded luckyCard”

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,7 +23,7 @@ public class CardTrick {
         
         Random rand=new Random();
         Scanner input=new Scanner(System.in);
-        
+        card luckyCard=new card();
        
         for (int i=0; i<magicHand.length; i++)
         { 
